b501c5375c1db4e46bfeeffec29cdb310235f8c4
SaraDJ1 <sarajedj@gmail.com>
2024-11-09 13:06:18 +0100

    zadatak

---
+++ trougao/izracunajC.py
@@ -0,0 +1,4 @@
+from math import sqrt
+def izracunaj_C(a,b):
+    c = sqrt(a*a+b*b)
+    return c
---
+++ trougao/main.py
@@ -0,0 +1,4 @@
+from izracunajC import izracunaj_C
+a = int(input("Unesite stranicu a"))
+b = int(input("Unesite stranicu b"))
+print(izracunaj_C(a,b))
